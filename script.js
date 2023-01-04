c28fc0fcf0f09b5b2da31776a4e505fe010ec120
Nischal <mailfornischal@gmail.com>
2023-01-05 01:19:46 +0530

    Mixpanel integration

--- script.js
+++ script.js
@@ -5,6 +5,91 @@ let navbar = document.querySelector(".navbar");
 let section = document.getElementsByTagName("section");
 let hexagon = document.getElementsByClassName("hexagon");
 
+//Events
+
+//Navbar
+mixpanel.track_links("#nav-about", "nav-about-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#nav-experience", "nav-experience-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#nav-work", "nav-work-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#nav-writings", "nav-writings-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#nav-contact", "nav-contact-clicked", {
+  "referrer": document.referrer
+});
+
+//Home
+mixpanel.track_links("#github-logo", "logo-github-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#linkedin-logo", "logo-linkedin-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#twitter-logo", "logo-twitter-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#instagram-logo", "logo-instagram-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#home-contact", "home-contact-clicked", {
+  "referrer": document.referrer
+});
+
+//About
+mixpanel.track_links("#about-jupiter-link", "about-jupiter-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#about-cv-download-clicked", "about-cv-download-clicked", {
+  "referrer": document.referrer
+});
+
+//Experience
+mixpanel.track_links("#experience-jupiter-link", "exprience-jupiter-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#experience-geekyants-link", "experience-geekyants-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#experience-litmusblox-link", "experience-litmusblox-clicked", {
+  "referrer": document.referrer
+});
+
+//Work
+mixpanel.track_links("#work-cryptobase-link", "work-cryptobase-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#work-newyorker-link", "work-newyorker-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#work-shopify-link", "work-shopify-clicked", {
+  "referrer": document.referrer
+});
+
+//Writings
+mixpanel.track_links("#writings-nftcollection-link", "writings-nftCollection-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#writings-blockchainapp-link", "writings-blockchainApp-clicked", {
+  "referrer": document.referrer
+});
+mixpanel.track_links("#writings-chromeextension-link", "writings-chromeExtension-clicked", {
+  "referrer": document.referrer
+});
+
+//Contact
+document.getElementById("send-message-btn").addEventListener("click", function() {
+  mixpanel.track('send-message-clicked', {
+    'source': "send message",
+  });
+});
+
+
 // Scroll Header Logic
 window.addEventListener("scroll", () => {
   header.classList.toggle("shadow", window.scrollY > 0);
