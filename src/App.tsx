cd47eb5f9289a8e20d3166aacd99021e97ec18d3
Nischal <mailfornischal@gmail.com>
2023-10-23 18:43:04 +0530

    fix: adding navigation/footer in Layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
 import './App.css';
 
-import Navigation from './sections/Navigation';
 import Hero from './sections/Hero';
 import About from './sections/About';
 import Work from './sections/Work';
 import Experiments from './sections/Experiments';
 import Writings from './sections/Writings';
 import Contact from './sections/Contact';
-import Footer from './sections/Footer';
 
 import Layout from './components/shared/Layout';
 
@@ -18,14 +16,12 @@ const App: React.FC = () => {
   return (
     <Theme>
       <Layout>
-        <Navigation />
         <Hero />
         <About />
         <Work />
         <Experiments />
         <Writings />
         <Contact />
-        <Footer />
       </Layout>
     </Theme>
   );
