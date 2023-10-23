9534a5c9e8fe1cfdbd09ffe1c35bbddee5d8f2a2
Nischal <mailfornischal@gmail.com>
2023-10-23 20:12:00 +0530

    temp: sidebar logic

--- src/sections/Navigation/index.tsx
+++ src/sections/Navigation/index.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './style.css';
 
 export interface NavigationProps {}
 
 const Navigation: React.FC<NavigationProps> = () => {
+  const [showSidebar, setShowSidebar] = useState<boolean>(false);
+
+  const handleSidebarToggle = useCallback(() => {
+    setShowSidebar((showSidebar) => !showSidebar);
+  }, []);
+
   return (
     <header>
       <nav>
         <a href="/" className="logo">
           nischal
         </a>
-        <div className="bx bx-menu" id="menu-icon"></div>
+        <button className="bx bx-menu" id="menu-icon" onClick={handleSidebarToggle}></button>
         <ul className="navbar">
           <li>
             <a id="nav-about" href="#about">
@@ -38,6 +44,35 @@ const Navigation: React.FC<NavigationProps> = () => {
             </a>
           </li>
         </ul>
+        {showSidebar ? (
+          <ul className="sidebar">
+            <li>
+              <a id="sidebar-about" href="#about">
+                about
+              </a>
+            </li>
+            <li>
+              <a id="sidebar-work" href="#work">
+                work
+              </a>
+            </li>
+            <li>
+              <a id="sidebar-experiments" href="#experiments">
+                experiments
+              </a>
+            </li>
+            <li>
+              <a id="sidebar-writings" href="#writings">
+                writings
+              </a>
+            </li>
+            <li>
+              <a id="sidebar-contact" href="#contact">
+                contact
+              </a>
+            </li>
+          </ul>
+        ) : null}
       </nav>
     </header>
   );
