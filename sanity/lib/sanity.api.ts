593e12f327e920941e2a10ff5a4d3e8e29b4c9a6
Nischal <mailfornischal@gmail.com>
2023-10-29 15:52:19 +0530

    feat: adding queries

--- sanity/lib/sanity.api.ts
+++ sanity/lib/sanity.api.ts
@@ -1,13 +1,16 @@
 export const useCdn = false
 
-export const dataset = assertValue(process.env.DATASET, 'Missing environment variable: DATASET')
+export const dataset = assertValue(
+  process.env.SANITY_STUDIO_DATASET,
+  'Missing environment variable: DATASET',
+)
 
 export const projectId = assertValue(
-  process.env.PROJECT_ID,
+  process.env.SANITY_STUDIO_PROJECT_ID,
   'Missing environment variable: PROJECT_ID',
 )
 
-export const readToken = process.env.SANITY_API_READ_TOKEN || ''
+export const readToken = process.env.SANITY_STUDIO_READ_TOKEN || ''
 
 export const apiVersion = process.env.API_VERSION || '2023-10-1'
 
--- sanity/lib/sanity.queries.ts
+++ sanity/lib/sanity.queries.ts
@@ -1,9 +1,85 @@
-export const navigationQuery = ``
-export const heroQuery = ``
-export const aboutQuery = ``
-export const workQuery = ``
-export const experimentsQuery = ``
-export const talksQuery = ``
-export const writingsQuery = ``
-export const contactQuery = ``
-export const footerQuery = ``
+export const navigationQuery = `*[_type == "navigation"][0]{
+  heading,
+  collection[]{
+    _key,
+    title,
+    href
+  }
+}`
+
+export const heroQuery = `*[_type == "hero"][0]{
+  socials[]{
+    _key,
+     url,
+    caption,
+    alt
+  },
+  greeting{
+    link,
+    text
+  },
+  cover{
+    asset
+  }
+}`
+
+export const aboutQuery = `*[_type == "about"]{
+  heading,
+  contact,
+  overview
+}`
+
+export const workQuery = `*[_type == "work"]{
+  heading,
+  collection[]{
+    org{
+      duration{
+        start,
+        end
+      },
+      image,
+      link,
+      description
+    }
+  }
+}`
+
+export const experimentsQuery = `*[_type == "experiments"][0]{
+  heading,
+  collection[]{
+    _key,
+    cover,
+    heading,
+    body
+  }
+}`
+
+export const talksQuery = `*[_type == "talks"][0]{
+  heading,
+  collection[]{
+    _key,
+    cover,
+    heading,
+    body
+  }
+}`
+
+export const writingsQuery = `*[_type == "writings"][0]{
+  heading,
+  collection[]{
+    _key,
+    image,
+    heading,
+    body,
+    link
+  }
+}`
+
+export const contactQuery = `*[_type == "contact"][0]{
+  heading,
+  text  
+}`
+
+export const footerQuery = `*[_type == "footer"][0]{
+  heading
+}`
