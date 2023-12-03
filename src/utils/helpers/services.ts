baa400f5b3fcb8a13d4e50ce5f9e6693e21e683a
Nischal <mailfornischal@gmail.com>
2023-12-03 18:37:44 +0530

    feat: updating end year logic.

--- src/utils/helpers/services.ts
+++ src/utils/helpers/services.ts
@@ -8,13 +8,10 @@ export const getStartDuration = (start: string) => {
 export const getEndDuration = (end: string) => {
   const today = Date.now();
 
-  const currentMonth = new Date(today).getMonth();
   const currentYear = new Date(today).getFullYear();
-
-  const startMonth = new Date(end).getMonth();
   const startYear = new Date(end).getFullYear();
 
-  if (currentMonth === startMonth && currentYear === startYear) {
+  if (currentYear === startYear) {
     return 'Present';
   }
 
