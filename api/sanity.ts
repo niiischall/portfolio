4bb3d278e36027ccda1e74cee3f3c493c1a208f9
Nischal <mailfornischal@gmail.com>
2025-02-06 22:56:39 +0530

    fix: add sanity file for proxy

---
+++ api/sanity.ts
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+
+export const proxyHandler = async (req, res) => {
+  try {
+    const response = await fetch(`https://${process.env.VITE_PROJECT_ID}.api.sanity.io/${process.env.VITE_API_VERSION}/data/query/${process.env.VITE_DATASET}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${process.env.VITE_API_TOKEN}`, // Authorization header
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch from Sanity API: ${response.statusText}`);
+    }
+
+    // Send the response back to the frontend
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    // Return error response if something goes wrong
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export default proxyHandler;
