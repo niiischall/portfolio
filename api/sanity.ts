b0fd4afb3ba74f772130ea18ce119ce0f1bbc422
Nischal <mailfornischal@gmail.com>
2025-02-06 23:19:11 +0530

    fix: remove comments

--- api/sanity.ts
+++ api/sanity.ts
@@ -5,7 +5,7 @@ export const proxyHandler = async (req, res) => {
     const response = await fetch(`https://${process.env.VITE_PROJECT_ID}.api.sanity.io/${process.env.VITE_API_VERSION}/data/query/${process.env.VITE_DATASET}?query=${req.query.query}`, {
       method: 'GET',
       headers: {
-        Authorization: `Bearer ${process.env.VITE_API_TOKEN}`, // Authorization header
+        Authorization: `Bearer ${process.env.VITE_API_TOKEN}`,
       },
     });
 
@@ -13,11 +13,9 @@ export const proxyHandler = async (req, res) => {
       throw new Error(`Failed to fetch from Sanity API: ${response.statusText}`);
     }
 
-    // Send the response back to the frontend
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
-    // Return error response if something goes wrong
     res.status(500).json({ error: error.message });
   }
 }
